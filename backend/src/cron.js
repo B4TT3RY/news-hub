aab83afe8184ca4043da18956455b50b2c1e292a
Lee Taehoon <gb4tt3ry@gmail.com>
2024-12-16 18:15:22 +0900

    Fix: cron 메시지 변경

--- backend/src/cron.js
+++ backend/src/cron.js
@@ -6,16 +6,16 @@ export const startCron = () => {
   console.log('Cron started');
   cron.schedule('0 * * * * *', async () => {
     if (ticker.activeSessions.length === 0 && scrapers.cache.length !== 0) {
-      scrapers.cache = [];
+      scrapers.cache.splice(0, scrapers.cache.length);
     }
 
     if (ticker.activeSessions.length === 0) return;
 
     const diff = await scrapers.updateCache();
     if (diff.length === 0) {
-      console.log('No new news');
       return;
     }
+    console.log(`[${new Date().toISOString()}] Find ${diff.length} new news, broadcasting...`)
     ticker.broadcast(diff);
   });
 };
